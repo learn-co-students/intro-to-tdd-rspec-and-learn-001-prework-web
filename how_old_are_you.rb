823e5e0bba957d7d6c5fd119d562a1871356784f
Mustafa Hill <Loma7@aol.com>
2016-01-27 18:02:09 -0800

    Done.

---
+++ how_old_are_you.rb
@@ -0,0 +1,8 @@
+require_relative  ' ./current_age_for_birth_year.rb'
+
+puts "What year were you born?"
+birth_year = gets.to_i 
+
+users = current_age_for_birth_year(birth_year)
+
+puts "You are: " + users_age.to_s + " years old."
